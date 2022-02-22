d9e884ae5a71e8fb847487cee568dfc9e19b1f98
Nicholas78L <nikolja191278@gmail.com>
2022-02-22 21:40:06 +0200

    Final commit for everything.

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
+import os
 import json
+from io import StringIO
 from google.cloud import bigquery
 from google.cloud import storage
 # pip install --upgrade google-cloud-storage
 
-SERVICE_ACCOUNT_JSON = r'/Users/a1/Desktop/gcp_test_key.json'
+SERVICE_ACCOUNT_JSON = '/Users/a1/Desktop/gsbq-key.json'   # path to key
 
-def write_in_local_file(file_name, data):
-    print(f'Data is: {data}')
-    with open(file_name, "w") as write_file:
-        json.dump(data, write_file)
+def write_from_source_local_file_to_destination_local_file(source_file, destination_file):
+    with open(source_file, "r") as read_file:
+        data = json.load(read_file)
+    result = [json.dumps(record) for record in data]
+    with open(destination_file, 'w') as obj:
+        for i in result:
+            obj.write(i + '\n')
 
-def upload_to_bucket(blob_name, path_to_file, bucket_name): #(как будет называться файл и путь к нему в бакете, путь исходного файла который хотим сохранить, бакет нэйм)
+# # # (как будет называться файл и путь к нему в бакете, путь исходного файла который хотим сохранить, бакет нэйм)
+def upload_to_bucket(blob_name, path_to_file, bucket_name):
     """ Upload data to a bucket"""
-
-    # Explicitly use service account credentials by specifying the private key
-    # file.
+    # # # Explicitly use service account credentials by specifying the private key file.
     storage_client = storage.Client.from_service_account_json(SERVICE_ACCOUNT_JSON)
-
-    #print(buckets = list(storage_client.list_buckets())
-
     bucket = storage_client.get_bucket(bucket_name)
     blob = bucket.blob(blob_name)
     blob.upload_from_filename(path_to_file)
@@ -27,41 +28,39 @@ def upload_to_bucket(blob_name, path_to_file, bucket_name): #(как будет 
 
 def gcs_to_bq():
     table_id = ''
-    # Construct a BigQuery client object.
+    # # # Construct a BigQuery client object.
     client = bigquery.Client.from_service_account_json(SERVICE_ACCOUNT_JSON)
-
     # TODO(developer): Set table_id to the ID of the table to create.
-    table_id = "scrappers-341616.gcp_test.example1"
-
+    # # # table_id = 'your_project_id.your_dataset-id.your_table_name'.
+    table_id = "gsbq-341910.cs_dataset_id.cs_bg_table"
     job_config = bigquery.LoadJobConfig(
         schema=[
-            bigquery.SchemaField("name", "STRING"),
-            bigquery.SchemaField("post_abbr", "STRING"),
+            bigquery.SchemaField("name", "STRING"),     # # # Поля Схемы должны соответствовать полям таблицы dataset в Google Cloud Storage
+            bigquery.SchemaField("price", "STRING"),
+            bigquery.SchemaField("sum1", "STRING"),
+            bigquery.SchemaField("quantity", "STRING"),
         ],
         source_format=bigquery.SourceFormat.NEWLINE_DELIMITED_JSON,
     )
-
-    uri = 'gs://gcp_example_bucket/gcp_testing/cloud_file.json'
-
+    uri = 'gs://wrjsontogcsbq/write_to_gc_bq/result_file.json'
     load_job = client.load_table_from_uri(
         uri,
         table_id,
-        location="US",  # Must match the destination dataset location.
+        location="us-west2",  # Must match the destination dataset location.
         job_config=job_config,
     )  # Make an API request.
-
     load_job.result()  # Waits for the job to complete.
-
     destination_table = client.get_table(table_id)
     print("Loaded {} rows.".format(destination_table.num_rows))
 
+            # # # Все функции вызывать последовательно, по одной (остальные закоменчивать).
 if __name__ == '__main__':
-    data = {
-        "name": "Zaphod Beeblebrox",
-        "post_abbr": "Betelgeusian Street"
-    }
+            # # # write_from...() эта ф-ция позволяет переформатировать JSON-й файл в ND-JSON файл (newline delimited JSON file) для распознания его таблицами BigQuery.
+    write_from_source_local_file_to_destination_local_file("aa_engl.json", 'nd-proceesed.json')  ## (откуда:файл-исходник, куда:будущий-файл-адресат)
+            # # #  os.system( copy /путь к исходнику данных    /куда положить в PyCharm и как назвать файл ) ф-ция копирует файл целиком.
+    # os.system('cp /Users/a1/PycharmProjects/write_to_CloudStorage/nd-proceesed.json /Users/a1/PycharmProjects/write_to_CloudStorage/temp/local_file.json')
+            # # #  upload_to_bucket(в какой dataset сохранить данные и как назвать файл (таблицу), откуда исходник, имя_баккета) ф-ция закачивает файл в bucket.
+    # upload_to_bucket('write_to_gc_bq/result_file.json', 'temp/local_file.json', 'wrjsontogcsbq')
+    # gcs_to_bq()
 
-    write_in_local_file('tmp/local_file.json', data)
-    upload_to_bucket('gcp_testing/cloud_file.json', 'tmp/local_file.json', 'gcp_example_bucket')
-    gcs_to_bq()
 
---
+++ main_1.py
@@ -0,0 +1,68 @@
+import os
+import json
+from google.cloud import bigquery
+from google.cloud import storage
+# pip install --upgrade google-cloud-storage
+
+SERVICE_ACCOUNT_JSON = '/Users/a1/Desktop/keyForWritingInGoogleCloudeBQ.json'   # path to key
+
+def write_in_local_file(file_name, data):
+    print(f'Data is: {data}')
+    with open(file_name, "w") as write_file:
+        json.dump(data, write_file)
+
+# (как будет называться файл и путь к нему в бакете, путь исходного файла который хотим сохранить, бакет нэйм)
+def upload_to_bucket(blob_name, path_to_file, bucket_name):
+    """ Upload data to a bucket"""
+    # Explicitly use service account credentials by specifying the private key
+    # file.
+    storage_client = storage.Client.from_service_account_json(SERVICE_ACCOUNT_JSON)
+    #print(buckets = list(storage_client.list_buckets())
+    bucket = storage_client.get_bucket(bucket_name)
+    blob = bucket.blob(blob_name)
+    blob.upload_from_filename(path_to_file)
+    print('blob.public_url', blob.public_url)
+    return blob.public_url
+
+def gcs_to_bq():
+    table_id = ''
+    # Construct a BigQuery client object.
+    client = bigquery.Client.from_service_account_json(SERVICE_ACCOUNT_JSON)
+    # TODO(developer): Set table_id to the ID of the table to create.
+    # table_id = 'your_project_id.your_dataset-id.your_table_name'.
+    table_id = "writejsontocloudstorage.write_to_cs_bq.wr_to_cs_bq_table"
+    job_config = bigquery.LoadJobConfig(
+        schema=[
+            bigquery.SchemaField("c_compiler_version", "STRING"),
+            bigquery.SchemaField("channel_targets", "STRING"),
+            bigquery.SchemaField("c_compiler", "STRING"),
+            bigquery.SchemaField("CONDA_BUILD_SYSROOT", "STRING"),
+            bigquery.SchemaField("target_platform", "STRING"),
+        ],
+        source_format=bigquery.SourceFormat.NEWLINE_DELIMITED_JSON,
+    )
+    uri = 'gs://wr_short_bucket/write_to_gc_bq/result_file_1.json'
+    load_job = client.load_table_from_uri(
+        uri,
+        table_id,
+        location="us-west2",  # Must match the destination dataset location.
+        job_config=job_config,
+    )  # Make an API request.
+    load_job.result()  # Waits for the job to complete.
+    destination_table = client.get_table(table_id)
+    print("Loaded {} rows.".format(destination_table.num_rows))
+
+if __name__ == '__main__':
+    data = {
+      "c_compiler_version": "10",
+      "channel_targets": "defaults",
+      "c_compiler": "clang",
+      "CONDA_BUILD_SYSROOT": "/opt/MacOSX10.10.sdk",
+      "target_platform": "osx-64"
+    }
+    # write_in_local_file('temp/local_file_1.json', data)
+    #                (куда сохранить данные и как назвать файл, откуда исходник, имя_баккета)
+    # upload_to_bucket('write_to_gc_bq/result_file_1.json', 'temp/local_file_1.json', 'wr_short_bucket')
+    gcs_to_bq()
+
+
